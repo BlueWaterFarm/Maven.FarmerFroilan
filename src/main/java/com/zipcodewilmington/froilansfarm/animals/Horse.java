df722609f7472df18cbeee16a106ca07f91a19ab
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-08-22 23:05:50 -0400

    seeing how all the things link together

--- src/main/java/com/zipcodewilmington/froilansfarm/animals/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/animals/Horse.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.froilansfarm.animals;
 
 
 import com.zipcodewilmington.froilansfarm.Rideable;
-import com.zipcodewilmington.froilansfarm.crops.Edible;
+
 
 public class Horse extends Animal implements Rideable {
 
